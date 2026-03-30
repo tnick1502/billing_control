622aee6ff7f59a10d5b0856ae043e5182ca512ec
tnick1502 <tnick1502@mail.ru>
2026-03-30 15:56:39 +0300

    feat: light version

---
+++ hard_start_lite.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+# Лёгкий стек: без Portainer и без MinIO Console (только S3). См. docker-compose.lite.yml
+docker rm $(docker ps -a -q) -f
+docker rmi $(docker images -a -q) -f
+docker system prune -a -f
+git pull
+docker compose -f docker-compose.lite.yml up -d --build
